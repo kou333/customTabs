828adce2a566ffcafb99d2ddca439144bccf0cf3
kou333 <naikkoustubh@gmail.com>
2024-12-16 14:55:01 +0530

    custom tabs

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import TabTest from './components/custom-tabs/tab-test';
+
+function App() {
+  return (
+    <div className="App">
+      
+<TabTest/>
+
+    </div>
+  );
+}
+
+export default App;
